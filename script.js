3cbef77ec33ed4368766bc273f8a8ad778301bc9
Krovannox <krovannox@proton.me>
2026-01-28 16:05:05 -0600

    Add eventListener to the buttons block
    Added the class "button" and data-value corresponding to each button.
    
    This is used to use a single eventListener for the block instead of each
    button, using dataset.value to get the data requiered for the functions.
    
    The class "button" is used to avoid undefined if the click happens
    inside the block but outside the button, now it returns if the target
    doesn't contain "button" as a class.

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+// Event listener for the buttons block
+const buttons = document.querySelector("#container-buttons");
+buttons.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("button")) return;
+
+    console.log(e.target.dataset.value);
+});
