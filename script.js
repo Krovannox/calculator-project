fa36b1dc14b8954a5950194a61ff810942c56f02
Krovannox <krovannox@proton.me>
2026-01-29 14:25:02 -0600

    Add function to "C" button

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+// Variables used for the calculation
 let a = 0;
 let operator = null;
 let b = null;
 
+// Select and give display an initial value
 let display = document.getElementById("display");
 display.value = a;
 
+// Clear function
+const clear = function() {
+    a = 0;
+    operator = null;
+    b = null;
+    display.value = a;
+}
+
+// Event listener for the buttons
 const buttons = document.getElementById("buttons-container");
 buttons.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("btn")) return;
-    
+    if (!e.target.classList.contains("btn")) return; // Don't act outside the target button
+
+    if (e.target.dataset.value === "clear") { // "C" button
+        clear();
+        return;
+    };
+
+    display.value += e.target.dataset.value; // Displays the clicked value
+
     console.log(e.target.dataset.value);
 });
