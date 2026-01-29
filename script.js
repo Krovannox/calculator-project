ad0e4ba2aa3fd076568410e5f6d27ec77e209eae
Krovannox <krovannox@proton.me>
2026-01-29 14:05:13 -0600

    Add eventListener to buttons and value to display
    The eventListener was added to the entire button block to avoid using
    individual eventListeners. It checks if the target contains the class
    of "btn" to function in order to prevent an event outside the direct
    button.
    
    Three variables where created for the calculations, leaving the
    "operator" and "b" as "null" by default. These will be used to clear
    the display and operations.
    
    Minor styling was added to the display for better visility.

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+let a = 0;
+let operator = null;
+let b = null;
+
+let display = document.getElementById("display");
+display.value = a;
+
+const buttons = document.getElementById("buttons-container");
+buttons.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("btn")) return;
+    
+    console.log(e.target.dataset.value);
+});
